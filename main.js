bf2fcfad38f6125205b276069cfc477a732a54cb
Simon Meza <simon.meza@correo.unimet.edu.ve>
2023-02-05 00:14:36 -0400

    Ahora si Hero listo con js y no con css

---
+++ main.js
@@ -0,0 +1,25 @@
+
+// Hero en js 
+let i = 0;
+let img = [];
+let time = 5000;
+
+img[0] = 'hero1.jpg';
+img[1] = 'hero3.jpg';
+img[2] = 'hero4.jpg';
+img[3] = 'hero6.jpg';
+
+
+function changeImg() {
+    document.slider.src = img[i];
+
+    if(i < img.length - 1){
+        i++;
+    } else {
+        i = 0;
+    }
+
+    setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
